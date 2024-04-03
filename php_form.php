5e97a6a10186cb71f4eae86323ea821b20113d79
Jalithc <126810658+Jalithc@users.noreply.github.com>
2024-04-03 13:30:09 +0530

    Add files via upload

---
+++ php_form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<form><pre>
+<label>First Name
+<input type="" name=""></label>
+
+<label for="Lname">Last Name</label>
+<input type="" name="" id="Lname">
+</form>
+<label>password</label>
+<input type="password" name="">
+</pre>
+</head>
+</html>
